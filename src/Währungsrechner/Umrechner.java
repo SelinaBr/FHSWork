8a55e723bb35140221cab3efa579498d8f5fe6e1
selin <selin@LAPTOP-7E1U8A3C.speedport.ip>
2020-05-14 20:51:28 +0200

    Rettung

--- src/Währungsrechner/Umrechner.java
+++ src/Währungsrechner/Umrechner.java
@@ -1,10 +1,7 @@
-package Währungsrechner;
+import java.util.ArrayList;
 import java.util.Scanner;
-
 import Währungsrechner.Currency;
-
 public class Umrechner {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -12,20 +9,63 @@ public class Umrechner {
 		Currency USDollar = new Currency("US-Dollar", "USD", 1.08);
 		Currency BritischePfund = new Currency("Pfund", "GBP", 0.978);
 		
+		ArrayList<Currency> currencyListe = new ArrayList<Currency>();
+		currencyListe.add(Euro);
+		currencyListe.add(USDollar);
+		currencyListe.add(BritischePfund);
+		
+		
+		
+
 		System.out.println("Willkommen bei Currency");
-		
-		System.out.println("Bitte geben Sie die Währung ein: ");
-		Scanner sc = new Scanner(System.in); 
-		String inputName = sc.nextLine();
+
 		
 		
-		System.out.println("Bitte geben Sie den Geldwert ein: ");
-		Scanner sc2 = new Scanner(System.in); 
-		double inputZahl = sc2.nextDouble();
-		
-		System.out.println("Ihre eingegebene Währung: "+inputName);
+		do {
+			System.out.println("Bitte geben Sie die Währung ein: ");
+			Scanner sc = new Scanner(System.in); 
+			String inputName = sc.nextLine();
+
+
+
+		for (Currency währungsname: currencyListe) {
+			if (währungsname.Währungsname.equals(inputName)) {
+				System.out.println("Ihre eingegebene Währung: "+inputName);
+			break;
+
+				}
+			}
+
+
+			System.out.println("Bitte geben Sie den Geldwert ein: ");
+			Scanner sc2 = new Scanner(System.in); 
+			double inputZahl = sc2.nextDouble();
+			System.out.println("Ihr eingegebener Geldwert: "+inputZahl);
+
+
+
+			System.out.println("Möchten Sie das Programm fortsetzen? Bitte geben sie 'j' für ja oder 'n' für nein ein: ");
+			Scanner sc3 = new Scanner(System.in);
+			String inputChar = sc3.nextLine();
+
+
+
+			if(inputChar == "j")	{
+				continue;
+			}
+
+			else if (inputChar.contentEquals("n"))  {
+					break;
+				}
+			else {
+				System.out.println("Diese Eingabe ist nicht zulässig!");
+			}
+
 		System.out.println("Ihr eingegebener Geldwert: "+inputZahl);
-		
+			
+
+
+		} while (true);
 	}
 
 }
